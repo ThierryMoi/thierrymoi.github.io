0246eafe24135d59250e911f756ae24a071e402c
HEMA THIERRY <olaf.kouamo@axian-group.com>
2023-05-02 17:24:37 +0000

    ok

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "e3d99d07fb6853f7453f763523115888",
-"index.html": "da1ff8bf1f7159acb9033dd96b55c07b",
-"/": "da1ff8bf1f7159acb9033dd96b55c07b",
+"index.html": "523b9418f1d1721abd13f9b2ae2a66c9",
+"/": "523b9418f1d1721abd13f9b2ae2a66c9",
 "main.dart.js": "8ac459711a4648cd2b518bcbd166cd0e",
-"flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -32,11 +31,7 @@ const RESOURCES = {
 "assets/assets/image/th.png": "4fb6f4000403fc24195cbd93b10d6440",
 "assets/assets/image/inout.png": "527d932b2aad15a4083cb5130d10d853",
 "assets/assets/fonts/agustina.otf": "7b9833076716a8d14eec0cf885a3153c",
-"assets/assets/fonts/montserrat.ttf": "ee6539921d713482b8ccd4d0d23961bb",
-"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
-"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
-"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
-"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e"
+"assets/assets/fonts/montserrat.ttf": "ee6539921d713482b8ccd4d0d23961bb"
 };
 
 // The application shell files that are downloaded before a service worker can
